3cb72b754ef3a114b1bdfc4707604c5eb3660963
Michael Jiang <MichaelxhJiang@users.noreply.github.com>
2018-09-16 00:49:02 -0400

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '../node_modules/react-image-gallery/styles/css/image-gallery-no-icon.css'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
